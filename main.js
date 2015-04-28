ee0e1122c2cd552a5c75760ebe1e06b58bda7ced
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-28 10:47:49 -0700

    quiz 2

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+
+	alert("Hey you clicked on me");
+
+	});
+
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+
+	});
+
+});
